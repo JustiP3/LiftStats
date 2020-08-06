4d5dff67a97f347837e895a83f3773f71a636b71
Justin <justinphil02@yahoo.com>
2020-08-06 09:24:12 -0700

    add react router. add welcome container component

---
+++ liftstats-frontend/src/containers/welcome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Welcome = () => {
+    return (
+      <div>
+        <button>Log In</button>
+        <button>Sign Up</button>
+        <p>Track your workouts the way you want to track them.</p>
+        <p>Then, get statistics and analysis on how you are progressing in your fitness journey.</p>
+      </div>
+    );
+  }
+   
+  export default Welcome;
