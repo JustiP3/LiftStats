70c9564981fd70362a065b4f88109ad2c3d96950
Justin <justinphil02@yahoo.com>
2020-08-06 13:46:11 -0700

    create migrations and validate in rails console

--- liftstats-backend/db/migrate/20200806203317_create_workouts.rb
+++ liftstats-backend/db/migrate/20200806203317_create_workouts.rb
@@ -1,7 +1,10 @@
 class CreateWorkouts < ActiveRecord::Migration[6.0]
   def change
     create_table :workouts do |t|
-
+      t.integer :user_id
+      t.string :workout_type
+      t.integer :weight
+      t.integer :reps
       t.timestamps
     end
   end
