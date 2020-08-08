8afb833cdab2df1224f26c2bf22cb740a85e7316
Justin <justinphil02@yahoo.com>
2020-08-08 12:21:49 -0700

    add StatsDashboard container component. add stats route

--- liftstats-frontend/src/App.js
+++ liftstats-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Welcome from './containers/Welcome'
 import Dashboard from './containers/Dashboard'
+import StatsDashboard from './containers/StatsDashboard'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <Route exact path="/" component={Welcome} />
       <Route exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/stats" component={StatsDashboard} />
     </Router>)
 }
 
---
+++ liftstats-frontend/src/containers/StatsDashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class StatsDashboard extends Component {
+
+    
+    render() {
+        return(<h1>Stats Dashboard</h1>)
+    }
+
+ 
+}
+
+
+     
+  
