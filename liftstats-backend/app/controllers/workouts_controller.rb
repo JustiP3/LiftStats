1631e8f19e2eaa6ad878e30ec2feb340651cc8de
Justin <justinphil02@yahoo.com>
2020-08-07 18:07:14 -0700

    add show action to both new controllers. add create to workouts

--- liftstats-backend/app/controllers/workouts_controller.rb
+++ liftstats-backend/app/controllers/workouts_controller.rb
@@ -1,2 +1,11 @@
 class WorkoutsController < ApplicationController
+
+    def show 
+        workout = Workout.find_by(id: params["id"])
+        render json: workout
+    end 
+
+    def create
+    end 
+
 end
