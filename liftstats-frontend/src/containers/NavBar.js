1fafb1e96638949f00312d22658cad6fa92db8a7
Justin <justinphil02@yahoo.com>
2020-08-08 12:47:34 -0700

    navbar links are working

--- liftstats-frontend/src/containers/NavBar.js
+++ liftstats-frontend/src/containers/NavBar.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 export default class NavBar extends Component {
 
     
     render() {
-        return(<h1>NavBar!</h1>)
+        return(
+        <h1>
+            <NavLink to="/dashboard">Dashboard</NavLink> | <NavLink to="/stats">Stats </NavLink>
+        </h1>)
     }
 
  
 }
+
+
+ 
 
