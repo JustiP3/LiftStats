108d8d0845d0d33c46324822139e8fe3a5141194
Justin <justinphil02@yahoo.com>
2020-08-09 15:10:24 -0700

    MostWeightGivenReps works when user selects a number of reps that has never been logged before

--- liftstats-frontend/src/components/MostWeightGivenReps.js
+++ liftstats-frontend/src/components/MostWeightGivenReps.js
@@ -14,7 +14,8 @@ export default class MostWeightGivenReps extends Component {
   
 
         return(
-            <div>
+            <div>    
+                <h3>Most Weight Lifted for a Given Rep Range (select a rep range)</h3>            
                 <form onSubmit={this.props.handleSubmitReps}>
                 <div className="form-group">
                     <label>Select Number of Reps</label>
@@ -22,6 +23,7 @@ export default class MostWeightGivenReps extends Component {
                 </div>
                     <button type="submit">Show Highest Weight Lifted for this Number of Reps</button>
                 </form>
+                <h4>Reps: {this.props.mostWeightGivenReps.reps}, Weight: {this.props.mostWeightGivenReps.weight}</h4>
             </div>
             
         )
--- liftstats-frontend/src/components/StatsCard.js
+++ liftstats-frontend/src/components/StatsCard.js
@@ -3,7 +3,7 @@ import MostWeightGivenReps from './MostWeightGivenReps'
 
 export default class StatsCard extends Component {
 
-    state={mostWeight: 0, mostReps: 0, mostWeightGivenReps:[{reps:0, weight: 0}]}
+    state={mostWeight: 0, mostReps: 0, mostWeightGivenReps:{reps:0, weight: 0}}
 
     componentDidMount() {
         fetch(`http://localhost:3000/users/1/workouts/${this.props.workout}/records`).then(resp => {
@@ -18,22 +18,30 @@ export default class StatsCard extends Component {
 
     render() {
 
-        const handleSubmitReps = () => {
-            fetch(`http://localhost:3000/users/1/workouts/${this.props.workout}/records/2`).then(resp => {
+        const handleSubmitReps = (event) => {
+            event.preventDefault() 
+
+            const reps = event.target.reps.value 
+        
+            fetch(`http://localhost:3000/users/1/workouts/${this.props.workout}/records/${reps}`).then(resp => {
                 return resp.json()
-            }).then(json => {
-                console.log(json.weight)
-                return this.setState({...this.state, mostWeightGivenReps:[{reps:2, weight: json.weight}]})
+            }).then(json => {  
+ 
+                if (!!json.most_weight_given_reps) {                
+                    return this.setState({...this.state, mostWeightGivenReps:{reps:json.most_weight_given_reps.reps, weight:json.most_weight_given_reps.weight}})
+                } else {
+                    return this.setState({...this.state, mostWeightGivenReps:{reps:reps, weight:"You have never logged this number of reps."}})
+                }           
+                
             })
         }
 
         return(
             <div key={this.props.workout} className="card">
-                <h3>{this.props.workout}</h3>
-                <p>Most Weight: {this.state.mostWeight.weight} for {this.state.mostWeight.reps} reps. ({this.state.mostWeight.created_at})</p>
-                <p>Most Reps: {this.state.mostReps.reps} lifting {this.state.mostReps.weight} lbs. ({this.state.mostReps.created_at})</p>
-                <p>Most Weight for a given number of reps:</p>
-                <MostWeightGivenReps handleSubmitReps={handleSubmitReps} />
+                <h1>{this.props.workout}</h1>
+                <h3>Most Weight: {this.state.mostWeight.weight} lbs.</h3><p>({this.state.mostWeight.created_at})</p>
+                <h3>Most Reps: {this.state.mostReps.reps} lifting {this.state.mostReps.weight} lbs.  </h3><p>({this.state.mostReps.created_at})</p>                
+                <MostWeightGivenReps mostWeightGivenReps={this.state.mostWeightGivenReps} handleSubmitReps={handleSubmitReps} />
             </div>
             
         )
