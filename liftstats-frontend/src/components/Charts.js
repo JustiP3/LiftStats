81b8166f727ac3c2a349b4958fe139df53b0b801
Justin <justinphil02@yahoo.com>
2020-08-12 19:52:11 -0700

    add recharts to Charts component

--- liftstats-frontend/src/components/Charts.js
+++ liftstats-frontend/src/components/Charts.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react'
+import { LineChart, Line } from 'recharts';
+
 
 export default class Charts extends Component {
-
    
 
     render() {
-
-      
+        
+        const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 350, pv: 2300, amt: 2400} ];
+        
+        const renderLineChart = (
+          <LineChart width={400} height={400} data={data}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          </LineChart>
+        );
 
         return(
             <div>
-               Charts!
+               {renderLineChart}
             </div>
         )
     }
