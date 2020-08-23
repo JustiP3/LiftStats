a5612d038e52dc9efbba96a4765ea7ca1f0cc290
Justin <justinphil02@yahoo.com>
2020-08-22 17:59:19 -0700

    logout button will dispatch the logout action. login will falsely log in users with an incorrect email

--- liftstats-frontend/src/containers/NavBar.js
+++ liftstats-frontend/src/containers/NavBar.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
-export default class NavBar extends Component {
 
+class NavBar extends Component {
     
     render() {
-        return(
-        <h1>
-            <NavLink to="/dashboard">Dashboard</NavLink> | <NavLink to="/stats">Stats </NavLink> | <NavLink to="/">Log Out</NavLink>
-        </h1>)
-    }
 
+        return(
+            <div>
+            <h1><NavLink to="/dashboard">Dashboard</NavLink> | <NavLink to="/stats">Stats </NavLink> | <button onClick={this.props.logout}> <NavLink to="/">Logout</NavLink></button></h1>
+            <h2>Current User: {this.props.displayName}</h2>
+            </div>
+        )
+    }
  
 }
 
+const mapStateToProps = state => ({displayName: state.displayName})
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch({type: 'LOGOUT'})
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar)
 
  
 
