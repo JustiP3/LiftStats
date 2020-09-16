13e43cbabd6eca096fedb808e923e10eb202c914
Justin <justinphil02@yahoo.com>
2020-09-15 19:09:02 -0700

    change MostWeightGivenStats to a functional component

--- liftstats-frontend/src/components/MostWeightGivenReps.js
+++ liftstats-frontend/src/components/MostWeightGivenReps.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 
-export default class MostWeightGivenReps extends Component {
-
-    render() {
-        
-        
-        return(
-            <div>    
-                <button onClick={this.props.handleClose}>Back</button>                      
-                <form onSubmit={this.props.handleSubmitReps}>
-                <div className="form-group">
-                    <label>Select Number of Reps</label>
-                    <input type="number" name="reps" min="1" max="50" />
-                    <button type="submit">Show Highest Weight Lifted for this Number of Reps</button>
-                </div>
-                   
-                </form>
-                
-                <h4>{this.props.mostWeightGivenReps.weight} lbs. for {this.props.mostWeightGivenReps.reps} reps</h4>
+function MostWeightGivenReps(props) {
+    return(
+        <div>    
+            <button onClick={props.handleClose}>Back</button>                      
+            <form onSubmit={props.handleSubmitReps}>
+            <div className="form-group">
+                <label>Select Number of Reps</label>
+                <input type="number" name="reps" min="1" max="50" />
+                <button type="submit">Show Highest Weight Lifted for this Number of Reps</button>
             </div>
+               
+            </form>
             
-        )
-    }
+            <h4>{props.mostWeightGivenReps.weight} lbs. for {props.mostWeightGivenReps.reps} reps</h4>
+        </div>
+        
+    )
 }
+
+export default MostWeightGivenReps
