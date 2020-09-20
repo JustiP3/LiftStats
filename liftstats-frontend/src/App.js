2c430d579986497f6df79e5781d65d0095143c38
Justin <justinphil02@yahoo.com>
2020-09-19 23:24:26 -0700

    changed App to a class component. start to follow an authentication tutorial for react-redux - currently broken

--- liftstats-frontend/src/App.js
+++ liftstats-frontend/src/App.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
 import Welcome from './containers/Welcome'
 import Dashboard from './containers/Dashboard'
 import StatsDashboard from './containers/StatsDashboard'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar'
 
-function App() {
-  return (
-    <Router>
-      <Route exact path="/" component={Welcome} />
-      <Route exact path="/dashboard" component={Dashboard} />
-      <Route exact path="/stats" component={StatsDashboard} />
-    </Router>)
+class App extends Component {
+  render() {
+
+    const { dispatch, isAuthenticated, errorMessage } = this.props
+
+    return (
+      <div>
+        <Navbar
+          isAuthenticated={isAuthenticated}
+          errorMessage={errorMessage}
+          dispatch={dispatch}
+        />
+
+      
+        <Router>
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/stats" component={StatsDashboard} />
+        </Router>
+
+      </div>
+      )
+    }
+  }
+
+// quote is from a tutorial and needs to be replaced with something relevant to this application 
+
+function mapStateToProps(state) {
+
+  const { quotes, auth } = state
+  const { quote, authenticated } = quotes
+  const { isAuthenticated, errorMessage } = auth
+
+  return {
+    quote,
+    isSecretQuote: authenticated,
+    isAuthenticated,
+    errorMessage
+  }
 }
 
-export default App;
+
+export default connect(mapStateToProps)(App)
---
+++ liftstats-frontend/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar(props)  {
+    
+        return(
+            <div>
+                <h4>NavBar </h4>
+                
+            </div>
+        )
+    
+}
